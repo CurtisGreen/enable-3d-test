87938038667c7a8a97451b17a882a462e42a8798
unknown <c.green5201@gmail.com>
2020-06-20 00:20:56 -0500

    Added full wasd movement and shift for sprinting. Removed slide after moving.

--- src/scenes/mainScene.js
+++ src/scenes/mainScene.js
@@ -1,13 +1,21 @@
 //import { Scene3D } from '../lib/enable3d/phaser-extension'
-const { enable3d, Scene3D, ExtendedObject3D, THREE, ThirdPersonControls, PointerLock, PointerDrag  } = ENABLE3D;
+const {
+    enable3d,
+    Scene3D,
+    ExtendedObject3D,
+    THREE,
+    ThirdPersonControls,
+    PointerLock,
+    PointerDrag,
+} = ENABLE3D;
 
-const isTouchDevice = 'ontouchstart' in window;
+const isTouchDevice = "ontouchstart" in window;
 
 export default class MainScene extends Scene3D {
     placementBox = {};
 
     constructor() {
-        super({ key: 'MainScene' })
+        super({ key: "MainScene" });
     }
 
     init() {
@@ -30,17 +38,17 @@ export default class MainScene extends Scene3D {
         this.third.camera.aspect = 2000 / 1000;
         this.third.camera.updateProjectionMatrix();
 
-        this.third.warpSpeed('-orbitControls');
+        this.third.warpSpeed("-orbitControls");
 
         this.third.add.box({ x: 1, y: 2 });
-        this.test = this.third.physics.add.box({ x: -1, y: 0.5});
+        this.test = this.third.physics.add.box({ x: -1, y: 0.5 });
         let test2 = this.third.physics.add.box({ x: 3, y: 0.5 });
         let test3 = this.third.physics.add.box({ x: -2, y: 0.5 });
 
         // Show where you will add a box
         this.placementBox = this.third.add.box(
-            { x: 1, y: 1 , z: 1, width: 1, height: 0.1},
-            {lambert: {color: 0x0000ff}},
+            { x: 1, y: 1, z: 1, width: 1, height: 0.1 },
+            { lambert: { color: 0x0000ff } }
         );
 
         // Create box on click
@@ -53,28 +61,28 @@ export default class MainScene extends Scene3D {
         });
 
         // Load Orc
-        this.third.load.gltf('assets/img/orc.glb').then(object => {
+        this.third.load.gltf("assets/img/orc.glb").then((object) => {
             const scene = object.scenes[0];
 
             this.orc = new ExtendedObject3D();
-            this.orc.name = 'orc';
+            this.orc.name = "orc";
             this.orc.add(scene);
             this.orc.position.set(7, 1, 0);
             this.third.add.existing(this.orc);
 
-            this.orc.traverse(child => {
+            this.orc.traverse((child) => {
                 if (child.isMesh) {
                     child.castShadow = child.receiveShadow = true;
                     child.material.metalness = 0;
                     child.material.roughness = 1;
                 }
-            })
+            });
 
             this.third.physics.add.existing(this.orc, {
-              shape: 'sphere',
-              radius: 0.25,
-              width: 0.5,
-              offset: { y: -0.15 }
+                shape: "sphere",
+                radius: 0.25,
+                width: 0.5,
+                offset: { y: -0.15 },
             });
             this.orc.body.setFriction(0.8);
             this.orc.body.setAngularFactor(0, 0, 0);
@@ -85,7 +93,7 @@ export default class MainScene extends Scene3D {
             this.controls = new ThirdPersonControls(this.third.camera, this.orc, {
                 offset: new THREE.Vector3(0, 1, 0),
                 targetRadius: 4, // Distance from character
-                sensitivity: {x: .11, y: .11},
+                sensitivity: { x: 0.11, y: 0.11 },
             });
 
             // Initial view angle
@@ -95,15 +103,17 @@ export default class MainScene extends Scene3D {
             if (!isTouchDevice) {
                 let pointerLock = new PointerLock(this.game.canvas);
                 let pointerDrag = new PointerDrag(this.game.canvas);
-                
-                pointerDrag.onMove(delta => {
-                    if (pointerLock.isLocked()) {
 
+                pointerDrag.onMove((delta) => {
+                    if (pointerLock.isLocked()) {
                         // Limit looking at sky
-                        if (this.controls.phi > -7 || (this.controls.phi <= -7 && delta.y > 0)) {
+                        if (
+                            this.controls.phi > -7 ||
+                            (this.controls.phi <= -7 && delta.y > 0)
+                        ) {
                             this.moveTop = -delta.y;
                         }
-                        
+
                         this.moveRight = delta.x;
                     }
                 });
@@ -114,23 +124,26 @@ export default class MainScene extends Scene3D {
 
         //this.third.haveSomeFun()
 
-        this.third.load.texture('assets/img/phaser-logo.png').then(grass => {
+        this.third.load.texture("assets/img/phaser-logo.png").then((grass) => {
             grass.wrapS = grass.wrapT = 1000; // RepeatWrapping
             grass.offset.set(0, 0);
             grass.repeat.set(2, 2);
 
             // BUG: To add shadows to your ground, set transparent = true
-            this.ground = this.third.physics.add.ground({ width: 20, height: 20, y: -1 }, { phong: { map: grass, transparent: true } });
+            this.ground = this.third.physics.add.ground(
+                { width: 20, height: 20, y: -1 },
+                { phong: { map: grass, transparent: true } }
+            );
         });
-
 
         // Controls
         this.keys = {
-            a: this.input.keyboard.addKey('a'),
-            w: this.input.keyboard.addKey('w'),
-            d: this.input.keyboard.addKey('d'),
-            s: this.input.keyboard.addKey('s'),
-            space: this.input.keyboard.addKey(32)
+            a: this.input.keyboard.addKey("a"),
+            w: this.input.keyboard.addKey("w"),
+            d: this.input.keyboard.addKey("d"),
+            s: this.input.keyboard.addKey("s"),
+            space: this.input.keyboard.addKey(32),
+            shift: this.input.keyboard.addKey("shift"),
         };
     }
 
@@ -138,13 +151,13 @@ export default class MainScene extends Scene3D {
         if (this.ground) {
             // Check line of sight to ground
             const raycaster = new THREE.Raycaster();
-            raycaster.setFromCamera({x: .1, y: .1}, this.third.camera);
+            raycaster.setFromCamera({ x: 0.1, y: 0.1 }, this.third.camera);
             const intersection = raycaster.intersectObject(this.ground);
 
             if (intersection.length != 0) {
                 let output = {
-                    x: Math.ceil(intersection[0].point.x), 
-                    y: Math.ceil(intersection[0].point.y), 
+                    x: Math.ceil(intersection[0].point.x),
+                    y: Math.ceil(intersection[0].point.y),
                     z: Math.ceil(intersection[0].point.z),
                 };
                 return output;
@@ -154,36 +167,37 @@ export default class MainScene extends Scene3D {
         } else {
             return false;
         }
-        
     }
 
     getMouseoverObjects() {
         // Check line of sight to obj
-        const {x, y} = this.getPointer();
+        const { x, y } = this.getPointer();
         const raycaster = new THREE.Raycaster();
         raycaster.setFromCamera({ x, y }, this.third.camera);
         const intersection = raycaster.intersectObjects([this.test, this.ground]);
 
         if (intersection.length != 0) {
-            console.log(intersection[0].point)
+            console.log(intersection[0].point);
             this.third.physics.add.box(intersection[0].point);
         }
-        
 
         // Clicked obj
         if (intersection.length > 0) {
             const obj = intersection[0].object;
-            
+
             if (obj != undefined) {
                 this.selected = obj;
-                this.selected.body.setCollisionFlags(2)
+                this.selected.body.setCollisionFlags(2);
 
                 if (this.curSelection != undefined) {
                     // this.third.destroy(this.curSelection);
                     this.curSelection.visible = false;
                 }
 
-                this.curSelection = this.third.add.box({x: Math.floor(x-1), y: Math.ceil(y)});
+                this.curSelection = this.third.add.box({
+                    x: Math.floor(x - 1),
+                    y: Math.ceil(y),
+                });
                 console.log(x, y);
             }
         }
@@ -194,10 +208,10 @@ export default class MainScene extends Scene3D {
 
         this.canJump = false;
         this.time.addEvent({
-            delay: 650,
+            delay: 1500,
             callback: () => {
                 this.canJump = true;
-            }
+            },
         });
         this.orc.body.applyForceY(6);
     }
@@ -241,13 +255,56 @@ export default class MainScene extends Scene3D {
             }
 
             // Move
-            const moveSpeed = 4;
-            if (this.keys.w.isDown || this.move) {
-                const x = Math.sin(theta) * moveSpeed,
-                    y = this.orc.body.velocity.y,
-                    z = Math.cos(theta) * moveSpeed;
+            let moveSpeed = 4;
+            let x = 0,
+                y = this.orc.body.velocity.y,
+                z = 0,
+                curAngle = 0,
+                forwards = false,
+                backwards = false;
 
+            // Forward/backwards
+            if (this.keys.w.isDown) {
+                forwards = true;
+                curAngle = theta;
+            } else if (this.keys.s.isDown) {
+                backwards = true;
+                curAngle = theta + Math.PI;
+            }
+
+            // Left
+            if (this.keys.a.isDown) {
+                if (forwards) {
+                    curAngle += Math.PI / 4;
+                } else if (backwards) {
+                    curAngle -= Math.PI / 4;
+                } else {
+                    curAngle = theta + Math.PI / 2;
+                }
+            }
+            // Right
+            else if (this.keys.d.isDown) {
+                if (forwards) {
+                    curAngle -= Math.PI / 4;
+                } else if (backwards) {
+                    curAngle += Math.PI / 4;
+                } else {
+                    curAngle = theta - Math.PI / 2;
+                }
+            }
+
+            // Shift for sprint
+            if (this.keys.shift.isDown) {
+                moveSpeed *= 2;
+            }
+
+            // Set player velocity
+            if (curAngle != 0) {
+                x = Math.sin(curAngle) * moveSpeed;
+                z = Math.cos(curAngle) * moveSpeed;
                 this.orc.body.setVelocity(x, y, z);
+            } else {
+                this.orc.body.setVelocity(0, y, 0);
             }
 
             // Jump
@@ -282,9 +339,9 @@ export default class MainScene extends Scene3D {
     getPointer() {
         // calculate mouse position in normalized device coordinates
         // (-1 to +1) for both components
-        const pointer = this.input.activePointer
-        const x = (pointer.x / this.cameras.main.width) * 2 - 1
-        const y = -(pointer.y / this.cameras.main.height) * 2 + 1
-        return { x, y }
+        const pointer = this.input.activePointer;
+        const x = (pointer.x / this.cameras.main.width) * 2 - 1;
+        const y = -(pointer.y / this.cameras.main.height) * 2 + 1;
+        return { x, y };
     }
 }
